1dcc9e23f4f5d18e44c009730007b57b2aac0c58
Devkrushn26 <devkrushndomadiya24@gmail.com>
2026-04-27 09:37:27 +0530

    remove unnessory files

--- scripts/supabase_amounts_to_decimal_usd.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-alter table public.expenses
-  alter column amount type numeric(12,6) using (amount::numeric / 100.0);
-
-alter table public.budgets
-  alter column amount type numeric(12,6) using (amount::numeric / 100.0);
-
-commit;
